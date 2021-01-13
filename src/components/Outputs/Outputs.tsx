6ab507ef20cd3d1c305707982cf201f0758d6ac2
danielpickett <daniel@danielpickett.me>
2021-01-12 22:20:07 -0500

    tweaking token output

--- src/components/Outputs/Outputs.tsx
+++ src/components/Outputs/Outputs.tsx
@@ -24,26 +24,7 @@ export const Outputs = ({
   const cssPaletteColors = parseScales(
     state.scales,
     (scale) =>
-      `  --color-${scale.scaleNameKebab}-${scale.shadeName}: ${scale.colorHex}`
-  )
-
-  const cssTextColorsOnGrey = textColors.reduce(
-    (stringOutput: string, scale) => {
-      const newString = scale.shades
-        .map((shade) => {
-          const len = 40
-          return (
-            `  --${shade.textColor.tokenName}:${gap(shade.textColor.tokenName, len)} ${shade.textColor.hex};\n` + // prettier-ignore
-            `  --${shade.textColorSubdued.tokenName}:${gap(shade.textColorSubdued.tokenName, len)} ${shade.textColorSubdued.hex};\n` + // prettier-ignore
-            `  --${shade.vividTextColor.tokenName}:${gap(shade.vividTextColor.tokenName, len)} ${shade.vividTextColor.hex};\n` + // prettier-ignore
-            `  --${shade.vividTextColorSubdued.tokenName}:${gap(shade.vividTextColorSubdued.tokenName, len)} ${shade.vividTextColorSubdued.hex};\n\n` // prettier-ignore
-          )
-        })
-        .join('')
-        .slice(0, -1) // removes trailing newline
-      return stringOutput + '\n' + newString
-    },
-    ''
+      `  --color-${scale.scaleNameKebab}-${scale.shadeName}: ${scale.colorHex};`
   )
 
   const cssTextColors = textColors
@@ -104,41 +85,55 @@ export const Outputs = ({
     cssTextColors +
     '}\n'
 
-  const sassPaletteColorAliases =
-    parseScales(
-      state.scales,
-      (x) =>
-        `$color-${x.scaleNameKebab}-${x.shadeName}: var(--color-${x.scaleNameKebab}-${x.shadeName});`
-    ) + '\n'
+  const sassPaletteColorAliases = parseScales(
+    state.scales,
+    (x) =>
+      `$color-${x.scaleNameKebab}-${x.shadeName}: var(--color-${x.scaleNameKebab}-${x.shadeName});`
+  )
 
-  const sassTextColorAliases = textColors.reduce(
-    (stringOutput: string, scale) => {
+  const sassTextColorAliases = textColors
+    .slice(1)
+    .reduce((stringOutput: string, scale) => {
+      const len = 40
       const newString = scale.shades
         .map((shade) => {
           return (
-            `$${shade.textColor.tokenName}: var(--${shade.textColor.tokenName});\n` +
-            `$${shade.textColorSubdued.tokenName}: var(--${shade.textColorSubdued.tokenName});\n` +
-            `$${shade.vividTextColor.tokenName}: var(--${shade.vividTextColor.tokenName});\n` +
-            `$${shade.vividTextColorSubdued.tokenName}: var(--${shade.vividTextColorSubdued.tokenName});\n\n`
+            // prettier-ignore
+            `$${shade.textColor.tokenName}:${gap(shade.textColor.tokenName, len)} var(--${shade.textColor.tokenName});\n` +
+            // prettier-ignore
+            `$${shade.textColorSubdued.tokenName}:${gap(shade.textColorSubdued.tokenName, len)} var(--${shade.textColorSubdued.tokenName});\n` +
+            // prettier-ignore
+            `$${shade.vividTextColor.tokenName}:${gap(shade.vividTextColor.tokenName, len)} var(--${shade.vividTextColor.tokenName});\n` +
+            // prettier-ignore
+            `$${shade.vividTextColorSubdued.tokenName}:${gap(shade.vividTextColorSubdued.tokenName, len)} var(--${shade.vividTextColorSubdued.tokenName});\n\n`
           )
         })
         .join('')
-        .slice(0, -1)
+        .slice(0, -1) // removes trailing newline
       return stringOutput + newString
-    },
-    ''
-  )
+    }, '')
 
   const sassVividTextColorsOnGreyAliases = vividTextColorsForGreyShades
-    .reduce((prevTokens: string, textColors) => {
-      const newTokens = textColors.vividTextColorSets
-        .map((textColorSet) => {
-          return (
-            `$${textColorSet.vividTextColor.tokenName}: var(--${textColorSet.vividTextColor.tokenName});\n` +
-            `$${textColorSet.vividTextColorSubdued.tokenName}: var(--${textColorSet.vividTextColorSubdued.tokenName});`
-          )
-        })
-        .join('\n')
+    .reduce((prevTokens: string, textColors, shadeIndex) => {
+      const len = 45
+      const greyOnGreyShade = greyTextColors.shades[shadeIndex].textColor
+      const greyOnGreyShadeSubdued =
+        greyTextColors.shades[shadeIndex].textColorSubdued
+      const newTokens =
+        // prettier-ignore
+        `$${greyOnGreyShade.tokenName}:${gap(greyOnGreyShade.tokenName, len )} var(--${greyOnGreyShade.tokenName});\n` +
+        // prettier-ignore
+        `$${greyOnGreyShadeSubdued.tokenName}:${gap( greyOnGreyShadeSubdued.tokenName, len )} var(--${greyOnGreyShadeSubdued.tokenName});\n` +
+        textColors.vividTextColorSets
+          .map((textColorSet) => {
+            const token = textColorSet.vividTextColor.tokenName
+            const tokenSubdued = textColorSet.vividTextColorSubdued.tokenName
+            return (
+              `$${token}:${gap(token, len)} var(--${token});\n` +
+              `$${tokenSubdued}:${gap(tokenSubdued, len)} var(--${tokenSubdued});`
+            )
+          })
+          .join('\n')
       return prevTokens + '\n' + newTokens + '\n'
     }, '')
     .slice(0, -1) // removes trailing newline
@@ -154,8 +149,8 @@ export const Outputs = ({
 
   return (
     <div className="Outputs">
-      <Output heading="CSS Custom Properties" content={cssCombined} />
-      <Output heading="SCSS Aliases" content={sassCombined} />
+      <Output heading="CSS Variables" content={cssCombined} />
+      <Output heading="Sass Aliases" content={sassCombined} />
       <Output heading="Config" content={configOutput} />
     </div>
   )
